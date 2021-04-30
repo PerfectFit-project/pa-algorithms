3bcdac839b6c9a6ce847373bcb4159fa88a222dc
Bouke Scheltinga <bscheltinga@users.noreply.github.com>
2021-04-30 14:55:53 +0200

    Changed library name back into pa-algorithms

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='paalgorithms',
+setup(name='pa-algorithms',
       version='0.1',
       description='Library with Perfect Fit PA algorithms',
       url='abc.com',
       author='Bouke Scheltinga',
       author_email='b.l.scheltinga@utwente.nl',
       license='MIT',
-      packages=['paalgorithms'],
+      packages=['pa-algorithms'],
       zip_safe=False)
