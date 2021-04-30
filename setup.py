207045836ae2df6a7be70abff57a370bc1f51e51
Bouke Scheltinga <bscheltinga@users.noreply.github.com>
2021-04-30 14:48:59 +0200

    Renamed folders to paalgorithms

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pa-algorithms',
+setup(name='paalgorithms',
       version='0.1',
       description='Library with Perfect Fit PA algorithms',
       url='abc.com',
       author='Bouke Scheltinga',
       author_email='b.l.scheltinga@utwente.nl',
       license='MIT',
-      packages=['pa-algorithms'],
+      packages=['paalgorithms'],
       zip_safe=False)
