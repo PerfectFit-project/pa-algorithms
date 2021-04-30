423d54de84cdfab2cb8f4fa29da4cb38d168c543
Bouke Scheltinga <b.scheltinga@rrd.nl>
2021-04-30 14:15:49 +0200

    Added email and url to setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(name='pa-algorithms',
       version='0.1',
       description='Library with Perfect Fit PA algorithms',
-      url='',
+      url='abc.com',
       author='Bouke Scheltinga',
       author_email='b.l.scheltinga@utwente.nl',
+      license='MIT',
       packages=['pa-algorithms'],
       zip_safe=False)
