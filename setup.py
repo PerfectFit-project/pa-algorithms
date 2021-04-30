ee0e5fa31aa438e59d8ee2beb1d30c1bba9735b2
Bouke Scheltinga <b.scheltinga@rrd.nl>
2021-04-30 13:48:05 +0200

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pa-algorithms',
+      version='0.1',
+      description='Library with Perfect Fit PA algorithms',
+      url='',
+      author='Bouke Scheltinga',
+      author_email='b.l.scheltinga@utwente.nl',
+      packages=['pa-algorithms'],
+      zip_safe=False)
