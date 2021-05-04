4d91ad827e73776f9bce7b323e7a1a904cfaf028
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2021-05-04 13:24:11 +0200

    Changed url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pa-algorithms',
       version='0.1',
       description='Library with Perfect Fit PA algorithms',
-      url='abc.com',
+      url='https://github.com/PerfectFit-project/pa-algorithms',
       author='Bouke Scheltinga',
       author_email='b.l.scheltinga@utwente.nl',
       license='MIT',
